145c9c3401f682971fd6141ee7b26d1fb51014be
Bikram Pal <worldscience27@gmail.com>
2023-10-21 23:41:48 +0530

    This is my quiz app for 2nd task of my internship .

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,44 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "How many players are there in one Baseball team?",
+            "Which company owns the Apple?",
+            "What is the height of Kanchenjunga?",
+            "When does National Science day celebrate?",
+            "How many MBs are there in 1 GB?",
+            "Where does the Bhil tribe find?",
+            "Which date is written in the preamble of India?",
+            "How long does a Test Match last?",
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"9","8","11","13"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"9,586 meter","8,586 meter","8,596 meter","8,576 meter"},
+            {"February 28","February 25","February 27","February 24"},
+            {"2024 MBs","1004 MBs","1024 KB","1024 MBs"},
+            {"Rajasthan","West Bengal","Delhi","Mumbai"},
+            {"November 26, 1949","November 26, 1948","November 26, 1946","November 26, 1945"},
+            {"5 days","1 days","9 days","3 days"},
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Apple",
+            "8,586 meter",
+            "February 28",
+            "1024 MBs",
+            "Rajasthan",
+            "November 26, 1949",
+            "5 days",
+    };
+
+}
